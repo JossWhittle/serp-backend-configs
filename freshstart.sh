86e848ce1b60e4d93e317fb24bbbacd999b0594c
Alex <a.s.lee@swansea.ac.uk>
2021-09-30 15:18:01 +0100

    This is also broken

--- freshstart.sh
+++ freshstart.sh
@@ -4,6 +4,7 @@ sudo rm -rf ceph/
 sudo rm -rf db2inst1/
 sudo rm -rf mssqlinst1/
 sudo rm -rf postgresinst1/
+sudo rm -rf minio/data*
 
 sudo mkdir -p ceph/varlibceph/
 sudo mkdir -p ceph/etcceph/
@@ -12,6 +13,7 @@ sudo mkdir -p mssqlinst1/data/
 sudo mkdir -p mssqlinst1/log/
 sudo mkdir -p mssqlinst1/secrets/
 sudo mkdir -p postgresinst1/data/
+sudo mkdir -p minio/data1-1  minio/data1-2  minio/data2-1  minio/data2-2  minio/data3-1  minio/data3-2  minio/data4-1  minio/data4-2
 
 if [ "$1" == "-r" ]; then
 	echo "Generating new data, this might take a while..."
